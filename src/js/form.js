9bfaa403d77b188a23f76efaf5be5aef39cbe690
johnny21-9 <heartless6m@gmail.com>
2021-04-07 21:52:10 +0300

    updated chat id

--- src/js/form.js
+++ src/js/form.js
@@ -6,8 +6,9 @@ $(document).ready(function () {
         let email = $("#user-email").val();
         let comment = $("#user-comment").val();
         let text = `<b>Имя пользователя:</b> ${name}%0A<b>Телефон:</b> ${tel}%0A<b>Email:</b> ${email}%0A<b>Комментарий:</b> ${comment}%0A`;
+        let chatID = "-565626597";
 
-        fetch(`https://api.telegram.org/bot1793783589:AAF2hxe9ZOEBEhDhWJh4ZcTEe4M34y4xmVk/sendMessage?chat_id=228944369&parse_mode=html&text=${text}`)
+        fetch(`https://api.telegram.org/bot1793783589:AAF2hxe9ZOEBEhDhWJh4ZcTEe4M34y4xmVk/sendMessage?chat_id=${chatID}&parse_mode=html&text=${text}`)
             .then( function (response) {
                 $('.backdrop').addClass("is-hidden");
                 $("#user-name").val('');
